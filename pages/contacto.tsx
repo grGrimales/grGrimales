bbc828304eb376ad441378cfda98f7de3fb5e7a4
Carlos Guzman <carlos.guzman3.contractor@bbva.com>
2022-06-27 09:04:32 -0500

    estructura incial de navbar

---
+++ pages/contacto.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Layout } from '../components/layout/Layout';
+import { Typography } from '@mui/material';
+
+const contacto = () => {
+  return (
+    <Layout
+    title='Contacto'
+    pageDescription='Ponte en contacto con Grediana Rojas'
+  >
+    <Typography variant='h1' component='h1'>Contacto</Typography>
+
+  </Layout>
+  )
+}
+
+export default contacto
