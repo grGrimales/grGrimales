c8226c49eecd2eb535d794f3a55a2b4bd3c11ccb
Carlos Guzman <carlos.guzman3.contractor@bbva.com>
2022-06-29 12:19:44 -0500

    se agrega carrusel al home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,43 @@
-import { CardActionArea, CardMedia, Divider, Card, Grid, Typography, CardContent, Hidden } from '@mui/material'
+import { Typography, Divider, Box } from '@mui/material';
 import type { NextPage } from 'next'
-import Image from 'next/image'
 import { Layout } from '../components/layout/Layout'
-
-
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 const Home: NextPage = () => (
-
-
   <>
     <Layout title={'Grediana Rojas - home'} pageDescription={'Portafolio desarrolladora front-end'}>
 
-      <CardActionArea>
-        <Typography variant='h1' component='h1'>Hola, soy Grediana Rojas,</Typography>
-        <Typography variant='h3' component='h3'> Desarrolladora Front-End</Typography>
 
-        <CardMedia image='/images/prueba3.png'
-          component='img'
-          sx={{ borderRadius: '5px' }}
-        />
-      </CardActionArea>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus= {false}
+        interval={7000}
+        showArrows={false}
+        
+      >
 
-      <Divider />
+        <Box mt={20} ml={5} mb={10} >
+          <Typography variant='h1' component='h1'  >Hola, soy Grediana Rojas.</Typography>
+          <Typography mt={2} mb={6} variant='h3' component='h3'> Desarrolladora Front-End.</Typography>
+          <Divider />
 
+        </Box>
+
+        <Box mt={20} ml={5} mb={10} >
+          <Typography variant='h1' component='h1'  >Hola, soy Grediana Rojas.</Typography>
+          <Typography mt={2} mb={6} variant='h3' component='h3'> Desarrolladora Front-End.</Typography>
+          <Divider />
+
+        </Box>
+
+
+      </Carousel>
 
     </Layout>
-
-
-
   </>
 )
 
-export default Home
+export default Home;
